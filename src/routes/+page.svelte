32fb281ee0de00f3946e70058ed7ad331dcba7e6
Tibet Tornaci <oofdere@hey.com>
2024-01-19 15:15:44 -0800

    axes are rendered outside canvas and select fix for chrome
    why would you not support `abs()` in css chrome why

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -39,22 +39,37 @@
 
 <div
 	class="canvas fixed left-0 top-0 h-0 w-0"
-	style="translate: {$canvasPosition[0]}px {$canvasPosition[1]}px; "
->
-	<div class="pointer-events-none fixed left-0 top-0 h-screen w-1 bg-green-500"></div>
-	<div class="pointer-events-none fixed left-0 top-0 h-1 w-screen bg-red-500"></div>
-</div>
+	style="translate: {$canvasPosition[0]}px {$canvasPosition[1]}px;"
+></div>
+
+<!-- X-axis marker-->
+<div
+	class="pointer-events-none fixed left-0 top-0 h-[1px] w-screen bg-red-500"
+	style="top: {$canvasPosition[1] - 0.5}px;"
+></div>
+
+<!-- Y-axis marker -->
+<div
+	class="pointer-events-none fixed left-0 top-0 h-screen w-[1px] bg-green-500"
+	style="left: {$canvasPosition[0] - 0.5}px;"
+></div>
 
 {#if $tool === 'select'}
 	<div
-		class="pointer-events-none fixed top-0 border-2 border-blue-600 bg-blue-400 opacity-35"
-		style="translate: {dragPos[0] - clickPos[0] > 0 ? clickPos[0] : dragPos[0]}px {dragPos[1] -
-			clickPos[1] >
-		0
-			? clickPos[1]
-			: dragPos[1]}px; width: abs({dragPos[0] - clickPos[0]}px); height: abs({dragPos[1] -
-			clickPos[1]}px);"
-	></div>
+		class="pointer-events-none fixed top-0 border-4 border-dashed border-blue-600 bg-blue-400 opacity-35"
+		style="translate: 
+		{dragPos[0] - clickPos[0] > 0 ? clickPos[0] : dragPos[0]}px 
+		{dragPos[1] - clickPos[1] > 0 ? clickPos[1] : dragPos[1]}px; 
+		width: {Math.abs(dragPos[0] - clickPos[0])}px; 
+		height: {Math.abs(dragPos[1] - clickPos[1])}px;"
+	>
+		<div class="fixed">
+			translate:
+			{dragPos[0] - clickPos[0] > 0 ? clickPos[0] : dragPos[0]}px
+			{dragPos[1] - clickPos[1] > 0 ? clickPos[1] : dragPos[1]}px; width: abs({dragPos[0] -
+				clickPos[0]}px); height: abs({dragPos[1] - clickPos[1]}px);
+		</div>
+	</div>
 {/if}
 
 <p>{$mouseLeft} {$canvasPosition}</p>
